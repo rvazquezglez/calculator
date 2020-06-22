48c84b297077db6fae9be1ec49c8d783bcfee2de
rvazquezglez <rvazquezglez@gmail.com>
2020-06-22 18:57:49 -0500

    Updates

---
+++ precache-manifest.69b634c5abf0f4badba21b45a5d86176.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b297fc8a43a3c9cfcde1fab2e1fb49d8",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "ff36a72a9652332ccd8b",
+    "url": "/calculator/static/css/main.3722a133.chunk.css"
+  },
+  {
+    "revision": "d282cc976b0fba12d390",
+    "url": "/calculator/static/js/2.ce2ecdc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.ce2ecdc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff36a72a9652332ccd8b",
+    "url": "/calculator/static/js/main.6778c3a6.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
